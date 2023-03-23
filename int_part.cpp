07051c3822b6c30f7459ae531760c6896a2823c9
Josip Ivica <josip.ivica@pm.me>
2023-03-23 20:45:37 +0100

    completed 3rd problem

--- int_part.cpp
+++ int_part.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+
+#define UPPER 1000
 
 using namespace std;
 
+void print_interval(int left, int right) {
+	cout << "[" << left << "," << right << "]" << endl;
+}
+
 int main() {
-	priority_queue<int, vector<int>, greater<int>> pq;
-	int x;
-	while (cin >> x)
-		if (x >= 0 && x <= 1000)
-			pq.push(x);
-	if (pq.top() != 0) 
-		cout << "[0," << pq.top() - 1 << "]" << endl;
-	while (!pq.empty()) {
-		int top = pq.top();
-		cout << "[" << top + 1 << "," << top - 1 << "]" << endl;
-		pq.pop();
+	vector<int> niz;
+	int unos;
+	while (cin >> unos)
+		if (unos >= 0 && unos <= UPPER) {
+			auto iter = niz.begin();
+			while (!niz.empty() && *iter > unos)
+				++iter;
+			niz.insert(iter, unos);
+		}
+	int current = 0;
+	for (auto it = niz.rbegin(); it != niz.rend(); ++it) {
+		if (*it > current)
+			print_interval(current, *it - 1);	
+		current = *it + 1;
 	}
+	if (current <= UPPER)
+		print_interval(current, UPPER);
 }
