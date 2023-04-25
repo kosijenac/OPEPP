5c33d45fcb2382f4e1027e4873442284f298c24e
Josip Ivica <josip.ivica@pm.me>
2023-04-25 16:09:04 +0200

    add comments

--- dz3/Graf.h
+++ dz3/Graf.h
@@ -18,12 +18,18 @@ public:
     Graf(Graf&&) noexcept;
     Graf& operator=(Graf&&) noexcept;
     ~Graf();
+    // Necemo setter za n jer bi mijenjanje n dok su bridovi vec postavljeni uzrokovalo nedef. ponasanje,
     int getBrVrhova() const;
     int getBrid(vrh, vrh) const;
     void setBrid(vrh, vrh, int);
     bool ima_vrh(vrh) const;
     const Graf& ispis() const;
     const Graf& optimalni(vrh, vrh) const;
+    /**
+     * Iako metode `ispis` i `optimalni` ne mijenjaju objekt, implementiramo i nekonstantne varijante
+     * kako bi mogle vratiti referencu na nekonstantni objekt kako bismo omogucili ulancavanje metoda
+     * koje mijenjaju objekt na objektima koji nisu const. Npr. `Obj.ispis().metodaKojaMijenjaObj();`
+     */
     Graf& ispis();
     Graf& optimalni(vrh, vrh);
 };
